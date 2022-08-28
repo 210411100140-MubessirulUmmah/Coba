5079de1ccb923ca33618fb39f6412461ab7646cb
210411100140-MubessirulUmmah <111749196+210411100140-MubessirulUmmah@users.noreply.github.com>
2022-08-28 19:06:20 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,33 @@
+# file: operator_aritmatika.py
+
+# Ambil input untuk mengisi nilai
+#incule 
+a = input("Inputkan nilai a: ")
+b = input("Inputkan nilai b: ")
+
+
+# Menggunakan operator penjumlahan
+c = a + b
+print ("Hasil %d + %d = %d" % (a, b, c))
+
+# Operator Pengurangan
+c = a - b
+print ("Hasil %d - %d = %d" % (a, b, c))
+
+# Operator Perkalian
+c = a * b
+print ("Hasil %d * %d = %d" % (a, b, c))
+
+# Operator Pembagian
+c = a / b
+print ("Hasil %d / %d = %d" % (a, b, c))
+
+# Operator Sisa Bagi
+c = a % b
+print ("Hasil %d %% %d = %d" % (a, b, c))
+
+# Operator Pangkat
+c = a ** b
+print ("Hasil %d ** %d = %d" % (a, b, c))
+
+
